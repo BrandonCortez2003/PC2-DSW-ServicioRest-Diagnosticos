6afac9289c839e86086c8588745b329cf794d206
DanielPRe15 <dani123pre@gmail.com>
2023-10-29 20:05:05 -0500

    Sistema Academia

---
+++ src/main/java/com/sistema/academia/SistemaAcademia2023Application.java
@@ -0,0 +1,13 @@
+package com.sistema.academia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAcademia2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaAcademia2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistema/academia/SistemaAcademia2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.academia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAcademia2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
